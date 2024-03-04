f4c32c69a2fe50049fd8a6328731e951d2b85802
Moses Alcantara <mosesalcantara123@gmail.com>
2024-03-04 16:54:50 +0800

    About Us UI

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -13,17 +13,17 @@
 @endsection
  
 @section('content')
-    <div class="container-fluid header">
-        <div class="row" style="background-image: url({{ asset('img/pages/about/about-header.jpg') }})">
+    <div class="container-fluid">
+        <div class="row header" style="background-image: url({{ asset('img/pages/about/about-header.jpg') }})">
             <div class="col">
                 <h1>About RLC Residences</h1>
             </div>
         </div>
     </div>
 
-    <div class="container-fluid about_div">
+    <div class="container-fluid about">
         <div class="row">
-            <div class="col description">
+            <div class="col">
                 <p>
                     Robinsons Land Corporation, a subsidiary of JG Summit Holdings, has been improving the lives of Filipinos for over 40 years.
                     Their success stems from understanding and adapting to the constantly emerging residential trends across their various developments.
@@ -38,45 +38,63 @@
         <div class="row">
             <div class="col text-center tagline">
                 <h4 class="mb-3">Discover what RLC Residences has to offer through its tagline</h4>
-                <div class="acronym">
-                    <h2>Raise</h2>
-                    <h2 class="center_h2">Live</h2>
-                    <h2>Connect</h2>
+                <img src="{{ asset('img/pages/about/tagline.png') }}" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col acronym_item">
+                <div class="row">
+                    <div class="col-8">
+                        <h4>Raise</h4>
+                        <p>
+                            'Raise' is about helping homeowners raise their game by providing beautiful and well-designed living spaces that they can proudly call theirs.
+                            RLC Residences ensures this exciting possibility by partnering with world-class designers to bring to life developments showcasing iconic features such as grand lobbies and home spaces with high ceilings equipped with upgraded deliverables where residents can happily live the lifestyle that they deserve.
+                        </p>
+                    </div>
+                    <div class="col">
+                        <img src="{{ asset('img/pages/about/raise-image.png') }}" alt="">
+                    </div>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col acronym_item">
-                <h4>Raise</h4>
-                <p>
-                    'Raise' is about helping homeowners raise their game by providing beautiful and well-designed living spaces that they can proudly call theirs.
-                    RLC Residences ensures this exciting possibility by partnering with world-class designers to bring to life developments showcasing iconic features such as grand lobbies and home spaces with high ceilings equipped with upgraded deliverables where residents can happily live the lifestyle that they deserve.
-                </p>
+                <div class="row">
+                    <div class="col">
+                        <img src="{{ asset('img/pages/about/live-image.png') }}" alt="">
+                    </div>
+                    <div class="col-8">
+                        <h4>Live</h4>
+                        <p>
+                            RLC Residences strives to continuously empower its homeowners to 'Live' and design their best life by integrating their lifestyles in every corner of the development.
+                            With well-balanced life as one of the priorities of homebuyers, RLC Residences ensures that its condominium projects have above-standard amenities - both in size and variety - that cater to their wellness, growth, and recreation.
+                            <br>
+                            Alongside this, living their best life is also supported by the right innovations that allow them to live a smart and production life.
+                            Coming from the evolving home and work trends, all RLC Residences units are also designed to have ample space to accomodate your own work-from-home-spaces, with efficient storage systems to make home life more convenient.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="col acronym_item">
-                <h4>Live</h4>
-                <p>
-                    RLC Residences strives to continuously empower its homeowners to 'Live' and design their best life by integrating their lifestyles in every corner of the development.
-                    With well-balanced life as one of the priorities of homebuyers, RLC Residences ensures that its condominium projects have above-standard amenities - both in size and variety - that cater to their wellness, growth, and recreation.
-                    <br>
-                    Alongside this, living their best life is also supported by the right innovations that allow them to live a smart and production life.
-                    Coming from the evolving home and work trends, all RLC Residences units are also designed to have ample space to accomodate your own work-from-home-spaces, with efficient storage systems to make home life more convenient.
-                </p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col acronym_item">
-                <h4>Connect</h4>
-                <p>
-                    More than anything, everyone values connection to people and places that matter.
-                    This is what 'Connect' stands for.
-                    In the quest towards living an efficient and value-oriented life, RLC Residencies developments are located in strategic areas, where transport hubs, major roads, and essential destinations are easily within reach.
-                    <br>
-                    To ensure that residents are connected to a convenient life at home, these properties are also equipped with smart home features and digital service platforms so they can easily manage their day-to-day living.
-                    All these translate to being at the center of opportunities so they can achieve their goals in life.
-                </p>
+                <div class="row">
+                    <div class="col-8">
+                        <h4>Connect</h4>
+                        <p>
+                            More than anything, everyone values connection to people and places that matter.
+                            This is what 'Connect' stands for.
+                            In the quest towards living an efficient and value-oriented life, RLC Residencies developments are located in strategic areas, where transport hubs, major roads, and essential destinations are easily within reach.
+                            <br>
+                            To ensure that residents are connected to a convenient life at home, these properties are also equipped with smart home features and digital service platforms so they can easily manage their day-to-day living.
+                            All these translate to being at the center of opportunities so they can achieve their goals in life.
+                        </p>
+                    </div>
+                    <div class="col">
+                        <img src="{{ asset('img/pages/about/connect-image.png') }}" alt="">
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -84,10 +102,10 @@
     
     <div class="container-fluid" style="background-image: url({{ asset('img/pages/home/video-bg.png') }})">
         <div class="row brand_story">
-            <div class="col embded-responsive text-center" >
+            <div class="col embded-responsive d-flex align-items-center justify-content-center">
                 <iframe class="embed-responsive-item video" src="https://www.youtube.com/embed/rsSbXE4Nf94" allowfullscreen></iframe>
             </div>
-            <div class="col brand_story_text">
+            <div class="col brand_story_text d-flex justify-content-center align-items-center">
                 <h2>Our Brand Story</h2>
                 <p>
                     As your partner in making your aspirations a reality, we uphold our commitment to delivering projects that raise your standard of living, empowers you to live your best life, and encourage you to nurture meaningful connections.
