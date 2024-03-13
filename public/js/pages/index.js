4f25b0189fe75bbd7a4eea5d3287eb2baf9281f2
Moses Alcantara <mosesalcantara123@gmail.com>
2024-03-13 16:53:31 +0800

    Search Start

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -1,4 +1,25 @@
 $(document).ready( function () {
+    // var header_data = [
+    //     {'picture': 'header-1.jpg', 'h3': 'Register My Unit'},
+    //     {'picture': 'header-2.jpg', 'h3': 'Check Available Units'},
+    //     {'picture': 'header-3.jpg', 'h3': 'Connect With Us'}, 
+    // ]
+
+    // var header_div = $('.header')
+
+    // for (var header of header_data) {
+    //     var col = $('<div>').addClass('col')
+    //     var picture = $('<div>').css({
+    //         'background-image': `url('img/pages/home/${header.picture}')`,
+    //         'background-size': 'cover',
+    //     })
+    //     var h3 = $('<h3>').html(header.h3)
+
+    //     picture.append(h3)
+    //     col.append(picture)
+    //     header_div.append(col)
+    // }
+
     $('.property_carousel_item').first().addClass('active')
     $('.video_carousel_item').first().addClass('active')
     $('.review_carousel_item').first().addClass('active')
@@ -18,4 +39,8 @@ $(document).ready( function () {
         var front = back.prev()
         front.css({'z-index': '0'})
     })
+
+    $('.header .col').click(function (e) { 
+
+    });
 })
