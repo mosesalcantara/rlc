947aee90517cad3acd2b01406b09ce118b32eb8a
Moses Alcantara <mosesalcantara123@gmail.com>
2024-03-19 16:15:51 +0800

    Responsive Design Home Page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -17,18 +17,22 @@
         <div class="row gx-0 header" style="background-image: url({{ asset('img/pages/home/shape.png') }})">
             
         </div>
+
+
     </div>
 
     <div class="container-fluid">
         <div class="row search mt-3">
-            <div class="col search_drops">
-                <div class="row">
-                    <div class="col-lg-3">
+            <div class="col">
+                <div class="row search_drops">
+                    <div class="col-xl-3">
                         <div class="dropdown" id='property_type'>
                             <button class="btn" type="button" aria-expanded="false">
                                 <div class="row">
-                                    <div class="col d-flex justify-content-start align-items-center">
+                                    <div class="col-10 d-flex justify-content-start align-items-center">
                                         <h6>Property Type</h6>
+                                    </div>
+                                    <div class="col-2 d-flex justify-content-end align-items-center">
                                         <i class="fa-solid fa-chevron-right"></i>
                                     </div>
                                 </div>
@@ -40,24 +44,28 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-3">
+                    <div class="col-xl-3">
                         <div class="dropdown" id='location'>
                             <button class="btn" type="button" aria-expanded="false">
                                 <div class="row">
-                                    <div class="col d-flex justify-content-start align-items-center">
+                                    <div class="col-10 d-flex justify-content-start align-items-center">
                                         <h6>Location</h6>
+                                    </div>
+                                    <div class="col-2 d-flex justify-content-end align-items-center">
                                         <i class="fa-solid fa-chevron-right"></i>
                                     </div>
                                 </div>
                             </button>
                         </div>
                     </div>
-                    <div class="col-lg-2">
+                    <div class="col-xl-2">
                         <div class="dropdown" id='type'>
                             <button class="btn" type="button" aria-expanded="false">
                                 <div class="row">
-                                    <div class="col d-flex justify-content-start align-items-center">
+                                    <div class="col-10 d-flex justify-content-start align-items-center">
                                         <h6>Type of Unit</h6>
+                                    </div>
+                                    <div class="col-2 d-flex justify-content-end align-items-center">
                                         <i class="fa-solid fa-chevron-right"></i>
                                     </div>
                                 </div>
@@ -71,12 +79,14 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-3">
+                    <div class="col-xl-3">
                         <div class="dropdown" id='rate'>
                             <button class="btn" type="button" aria-expanded="false">
                                 <div class="row">
-                                    <div class="col d-flex justify-content-start align-items-center">
+                                    <div class="col-10 d-flex justify-content-start align-items-center">
                                         <h6>Rental Rate</h6>
+                                    </div>
+                                    <div class="col-2 d-flex justify-content-end align-items-center">
                                         <i class="fa-solid fa-chevron-right"></i>
                                     </div>
                                 </div>
@@ -90,9 +100,10 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-1 d-flex justify-content-center align-items-center search_btn">
+                    <div class="col-xl-1 d-flex justify-content-center align-items-center search_btn">
                         <button class="btn btn-warning">
                             <i class="fa fa-search"></i>
+                            <h6>Find My Unit</h6>
                         </button>
                     </div>
                 </div>
@@ -112,10 +123,10 @@
 
     <div class="container-fluid featured">
         <div class="row">
-            <div class="col">        
+            <div class="col-xl-6 d-flex align-items-center justify-content-start">        
                 <h1>Our Featured Properties</h1>
             </div>
-            <div class="col text-end">
+            <div class="col-xl-6 d-flex align-items-center justify-content-end">
                 <h4>Explored our wide selection of projects to know more.</h4>
             </div>
         </div>
@@ -130,7 +141,7 @@
                             <div class="card_front">
                                 <div class="d-flex justify-content-center align-items-end" style="background-image: url({{ asset('uploads/properties/pictures') }}/{{ $property['picture'] }})">
                                     {{-- <img class="logo" src="{{ asset('uploads/properties/logos') }}/{{ $property['logo'] }}" alt=""> --}}
-                                    <i class="fa-solid fa-circle-chevron-right fa-4x front_switch"></i>
+                                    <i class="fa-solid fa-circle-chevron-right front_switch"></i>
                                 </div>
                             </div>
                             <div class="card_back">
@@ -142,10 +153,10 @@
 
                                     <div class='col details'>
                                         <div class="d-flex justify-content-end">
-                                            <i class="fa-solid fa-circle-chevron-left fa-3x back_switch text-info"></i>
+                                            <i class="fa-solid fa-circle-chevron-left back_switch text-info"></i>
                                         </div>
 
-                                        <h3>{{ $property['name'] }}</h3>
+                                        <h4>{{ $property['name'] }}</h4>
                                         <p>{{ $property['description'] }}</p>
 
                                         <div>
@@ -172,8 +183,8 @@
                     </div>
 
                     <div class="carousel_controls d-flex justify-content-center">
-                        <i class="fa-solid fa-chevron-left fa-2x" data-bs-target="#properties_carousel" data-bs-slide="prev"></i>
-                        <i class="fa-solid fa-chevron-right fa-2x" data-bs-target="#properties_carousel" data-bs-slide="next"></i>
+                        <i class="fa-solid fa-chevron-left" data-bs-target="#properties_carousel" data-bs-slide="prev"></i>
+                        <i class="fa-solid fa-chevron-right" data-bs-target="#properties_carousel" data-bs-slide="next"></i>
                     </div>
                 </div>
             </div>
@@ -194,9 +205,9 @@
                 @endforeach
                 </div>
 
-                <div class="justify-content-center">
-                    <i class="fa-solid fa-circle-chevron-left fa-4x" data-bs-target="#videos_carousel" data-bs-slide="prev"></i>
-                    <i class="fa-solid fa-circle-chevron-right fa-4x" data-bs-target="#videos_carousel" data-bs-slide="next"></i>
+                <div class="d-flex justify-content-center">
+                    <i class="fa-solid fa-circle-chevron-left" data-bs-target="#videos_carousel" data-bs-slide="prev"></i>
+                    <i class="fa-solid fa-circle-chevron-right" data-bs-target="#videos_carousel" data-bs-slide="next"></i>
                 </div>
             </div>
         </div>
