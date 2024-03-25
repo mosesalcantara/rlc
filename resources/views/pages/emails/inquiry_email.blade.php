17f751c551c052d694644ec532a88be4dcd7fb81
Moses Alcantara <mosesalcantara123@gmail.com>
2024-03-25 15:03:46 +0800

    Simple Email Design

--- resources/views/pages/emails/inquiry_email.blade.php
+++ resources/views/pages/emails/inquiry_email.blade.php
@@ -2,12 +2,37 @@
 <html>
 <head>
     <title>RLC Leasing</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <style>
+        .card {
+            border-radius: 0.7rem;
+            border: 0.15rem solid black;
+            width: 20vw;
+        }
+
+        .card-body {
+            padding: 0 3rem 0;
+        }
+    </style>
 </head>
 <body>
-    <h1>{{ $mailData['title'] }}</h1><br>
-    <h3>Fullname: {{ $mailData['fullname'] }}</h3>
-    <h3>Email: {{ $mailData['email'] }}</h3>
-    <h3>Contact Number: {{ $mailData['number'] }}</h3><br>
-    <p>{{ $mailData['body'] }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h1>{{ $mailData['title'] }}</h1>
+                        <h3>Fullname: {{ $mailData['fullname'] }}</h3>
+                        <h3>Email: {{ $mailData['email'] }}</h3>
+                        <h3>Contact Number: {{ $mailData['number'] }}</h3>
+                        <p>{{ $mailData['body'] }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -6,27 +6,36 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
   <style>
-    .container {
-  position: relative;
-  text-align: center;
-  color: white;
-}
-    .centered {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+    .card {
+        border-radius: 0.7rem;
+        border: 0.15rem solid black;
+        width: 30vw;
+    }
+
+    .card-body {
+        padding: 0 3rem 0;
+    }
   </style>
 </head>
 <body>
-    <div class="container">
-        <img src="{{ asset('/img/pages/for_lease/residential.png') }}" alt="">
-
-        <div class="centered">
-            Residential
+  <div class="container">
+    <div class="row">
+      <div class="col d-flex justify-content-center">
+        <div class="card">
+          <div class="card-body">
+              <h1 class="text-center">Residential Inquiry</h1><br>
+              <h3>Fullname: Moses Alcantara</h3>
+              <h3>Email: mosesalcantara123@gmail.com</h3>
+              <h3>Contact Number: 09685913442</h3><br>
+              <p>This is just a test message</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </body>
 </html>
