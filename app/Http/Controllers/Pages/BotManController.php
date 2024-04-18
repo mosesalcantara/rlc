f7b1146551f1bde8b24f348a0c2a01fd49229bcd
Moses Alcantara <mosesalcantara123@gmail.com>
2024-04-18 10:14:25 +0800

    Botman Unit Queries and Design

--- app/Http/Controllers/Pages/BotManController.php
+++ app/Http/Controllers/Pages/BotManController.php
@@ -5,37 +5,89 @@ namespace App\Http\Controllers\Pages;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Conversations\UnitConversation;
+use App\Models\Property;
 
 class BotManController extends Controller
 {
     public function botman() {
         $botman = app('botman');
    
-        $botman->hears('{message}', function($botman, $message) {
-   
-            if ($message == 'hi') {
-                $this->ask_category($botman);
-            }
-            
-            else{
-                $botman->reply("Start a conversation by saying hi.");
-            }
-   
+        $botman->hears('hi', function($botman) {
+            $botman->reply('What are you looking for?');
         });
-   
-        $botman->listen();
-    }
+
+        $botman->hears('units', function($botman) {
+            $botman->reply('For Sale or For Lease?');
+        });
+
+        $botman->hears('sale', function($botman) {
+            $botman->reply('Pre-selling or RFO Units?');
+        });
+
+        $botman->hears('pre-selling', function($botman) {
+            $reply = '<p>Here is a list of available units:<p>';
+
+            $where = [
+                'sale_status' => 'Pre-Selling',
+                'retail_status' => 'For Sale',
+            ];
     
-    public function ask_category($botman)
-    {
-        $botman->ask('What are you looking for?', function($answer) {
-   
-            $category = $answer->getText();
-   
-            if ($category == 'units') {
-                $this->say("Okay searching $category");
+            $sale_units = Property::join('residential_units', 'properties.id', '=', 'residential_units.property_id')->where($where)->orderBy('properties.name')->limit(10)->get();
+            foreach ($sale_units as $sale_unit) {
+                $reply .= "<a href='/for-sale/category/pre-selling/$sale_unit->id' target='_blank'>$sale_unit->name - $sale_unit->unit_id</a><br>";
             }
+
+            $botman->reply($reply);
         });
+
+        $botman->hears('rfo', function($botman) {
+            $reply = '<p>Here is a list of available units:<p>';
+
+            $where = [
+                'sale_status' => 'RFO',
+                'retail_status' => 'For Sale',
+            ];
+    
+            $sale_units = Property::join('residential_units', 'properties.id', '=', 'residential_units.property_id')->where($where)->orderBy('properties.name')->limit(10)->get();
+            foreach ($sale_units as $sale_unit) {
+                $reply .= "<a href='/for-sale/category/rfo/$sale_unit->id' target='_blank'>$sale_unit->name - $sale_unit->unit_id</a><br>";
+            }
+
+            $botman->reply($reply);
+        });
+
+        $botman->hears('lease', function($botman) {
+            $botman->reply('Residential or Commercial Units?');
+        });
+
+        $botman->hears('residential', function($botman) {
+            $reply = '<p>Here is a list of available units:<p>';
+
+            $where = [
+                'retail_status' => 'For Lease',
+                'published' => 1,
+            ];
+
+            $r_units = Property::join('residential_units', 'properties.id', '=', 'residential_units.property_id')->where($where)->orderBy('properties.name')->limit(10)->get();
+            foreach ($r_units as $r_unit) {
+                $reply .= "<a href='/for-lease/category/residential_units/$r_unit->id' target='_blank'>$r_unit->name - $r_unit->unit_id</a><br>";
+            }
+
+            $botman->reply($reply);
+        });
+
+        $botman->hears('commercial', function($botman) {
+            $reply = '<p>Here is a list of available units:<p>';
+
+            $c_units = Property::join('commercial_units', 'properties.id', '=', 'commercial_units.property_id')->orderBy('properties.name')->limit(10)->get();
+
+            foreach ($c_units as $c_unit) {
+                $reply .= "<a href='/for-lease/category/commercial_units/$c_unit->id' target='_blank'>$c_unit->name - $c_unit->retail_id</a><br>";
+            }
+
+            $botman->reply($reply);
+        });
+
+        $botman->listen();
     }
 }
