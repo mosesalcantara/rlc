d3f747d426284afd76ad5a241c4e14abcdefd459
Moses Alcantara <mosesalcantara123@gmail.com>
2024-04-30 11:54:38 +0800

    Dashboard Controller New Data

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,6 +10,9 @@ use App\Models\Property;
 use App\Models\ResidentialUnit;
 use App\Models\CommercialUnit;
 use App\Models\ParkingSlot;
+use App\Models\InquiryEmail;
+use App\Models\Viewing;
+use App\Models\RegisteredUnit;
 
 class AdminController extends Controller
 {
@@ -20,9 +23,33 @@ class AdminController extends Controller
             $counts['residential_units'] = ResidentialUnit::all()->count();
             $counts['commercial_units'] = CommercialUnit::all()->count();
             $counts['parking_slots'] = ParkingSlot::all()->count();
+            $counts['inquiries'] = InquiryEmail::all()->count();
+            $counts['viewings'] = Viewing::all()->count();
+            $counts['registered_units'] = RegisteredUnit::all()->count();
+
+            $sale = ResidentialUnit::all()->where('retail_status', 'For Sale')->count();
+            $lease = ResidentialUnit::all()->where('retail_status', 'For Lease')->count();
+            $retail_status = [
+                'sale' => $sale,
+                'lease' => $lease,
+            ];
+
+            $for_lease = [
+                'residential_units' => $counts['residential_units'],
+                'commercial_units' => $counts['commercial_units'],
+                'parking_slots' => $counts['parking_slots'],
+            ];
+
+            $amenities_property = Property::selectRaw('properties.id, properties.name, (Select Count(id) from Amenities Where property_id = properties.id) As amenities')->get();
+
+            $reviews_property = Property::selectRaw('properties.id, properties.name, (Select Count(id) from Reviews Where property_id = properties.id) As reviews')->get();
 
             $data = [
                 'counts' => $counts,
+                'retail_status' => $retail_status,
+                'for_lease' => $for_lease,
+                'amenities_property' => $amenities_property,
+                'reviews_property' => $reviews_property,
             ];
 
             return view('admin.index')->with('data', $data);
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,6 +20,8 @@
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     </div>
 
+    {{-- {{ dd($data) }} --}}
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
