34e4850ad17703c025375b075ae8a5401ffb058c
Moses Alcantara <mosesalcantara123@gmail.com>
2024-04-30 11:57:15 +0800

    Added Chart JS

--- resources/views/sections/admin/layout.blade.php
+++ resources/views/sections/admin/layout.blade.php
@@ -275,6 +275,8 @@
         <!-- Custom scripts for all pages-->
         <script src="{{ asset('js/admin/sb-admin-2.js') }}"></script>
 
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/dc08c6c264.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/2.0.1/js/dataTables.js"></script>
